4db1df00bf759c64103444d61392f649b7fc0458
Stefan Armbruster <stefan@armbruster-it.de>
2013-09-20 07:42:06 +0200

    render runningQueries in json too

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -2,6 +2,7 @@ import org.neo4j.ratpack.CypherHandler
 import org.neo4j.ratpack.Neo4jModule
 import org.neo4j.ratpack.QueryRegistry
 
+import static groovy.json.JsonOutput.toJson
 import static org.ratpackframework.groovy.RatpackScript.ratpack
 import static org.ratpackframework.groovy.Template.groovyTemplate
 
@@ -20,11 +21,19 @@ ratpack {
         // register handler based on path and all methods
         handler "cypher", registry.get(CypherHandler)
 
+        // handler for runningqueries in html and json
         get('runningQueries') { QueryRegistry queryRegistry ->
             if (request.queryParams.key) {
                 // TODO: terminate query
             }
-            render groovyTemplate("runningQueries.html", runningQueries: queryRegistry.runningQueries)
+            respond byContent.html {
+                render groovyTemplate("runningQueries.html", runningQueries: queryRegistry.runningQueries)
+            }.json {
+                def listForJson = queryRegistry.runningQueries.collect { String key, QueryRegistry.QueryMapEntry value ->
+                    [key: key, thread: value.thread.id, started: value.started, cypher: value.cypher]
+                }
+                response.send toJson(listForJson)
+            }
         }
 
         assets "public"
