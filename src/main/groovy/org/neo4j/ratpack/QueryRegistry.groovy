9f8b11f3331e5f443c3a1d51e90e06fa881a2f9b
Stefan Armbruster <stefan@armbruster-it.de>
2013-09-21 22:21:30 +0200

    fix NPE when aborting a non-existing query, add logging

--- src/main/groovy/org/neo4j/ratpack/QueryRegistry.groovy
+++ src/main/groovy/org/neo4j/ratpack/QueryRegistry.groovy
@@ -3,14 +3,15 @@ package org.neo4j.ratpack
 import com.google.inject.Inject
 import groovy.transform.CompileStatic
 import groovy.transform.EqualsAndHashCode
-import org.neo4j.graphdb.GraphDatabaseService
-import org.neo4j.kernel.GraphDatabaseAPI
+import groovy.transform.ToString
+import groovy.util.logging.Log
 import org.neo4j.kernel.guard.Guard
 import org.ratpackframework.session.SessionIdGenerator
 
 import java.util.concurrent.ConcurrentHashMap
 
 @CompileStatic
+@Log
 class QueryRegistry {
 
     private final ConcurrentHashMap<String, QueryMapEntry> runningQueries = new ConcurrentHashMap<String, QueryMapEntry>()
@@ -28,21 +29,29 @@ class QueryRegistry {
         VetoGuard vetoGuard = new VetoGuard()
         guard.start(vetoGuard)
         runningQueries.put(key, new QueryMapEntry(cypher: cypher, vetoGuard: vetoGuard))
+        log.info "registered query $cypher ($key)"
         key
     }
 
     public QueryMapEntry unregisterQuery(String key) {
         guard.stop()
+        log.info "unregistered query ($key)"
         runningQueries.remove(key)
     }
 
     QueryMapEntry abortQuery(String key) {
         QueryMapEntry entry = runningQueries[key]
+        if (entry==null) {
+            throw new IllegalArgumentException("no query running with key $key")
+        }
         entry.vetoGuard.abort = true
+        log.info "aborted query ($key)"
+
         entry
     }
 
     @EqualsAndHashCode //(includes = ["cypher", "started", "thread"])
+    @ToString
     static class QueryMapEntry {
         String cypher
         Date started = new Date()
--- src/main/groovy/org/neo4j/ratpack/VetoGuard.groovy
+++ src/main/groovy/org/neo4j/ratpack/VetoGuard.groovy
@@ -2,7 +2,6 @@ package org.neo4j.ratpack
 
 import groovy.transform.CompileStatic
 import org.neo4j.kernel.guard.Guard
-import org.neo4j.kernel.guard.GuardException
 
 @CompileStatic
 class VetoGuard implements Guard.GuardInternal {
@@ -12,7 +11,6 @@ class VetoGuard implements Guard.GuardInternal {
     @Override
     void check() {
         if (abort) {
-//            abort = true
             throw new VetoGuardException("aborted")
         }
     }
